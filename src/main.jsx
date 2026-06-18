ecca5e8ceb6526b0374be78cc965ad1657f93819
Haider010 <haiderkarar.dev@gmail.com>
2026-06-18 18:22:31 +0500

    refactor: Remove hideGhlHeader utility and related styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,5 @@
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
-import { hideGhlHeader } from "./utils/hideGhlHeader";
 import "./styles.css";
-
-hideGhlHeader();
 
 createRoot(document.getElementById("root")).render(<App />);
