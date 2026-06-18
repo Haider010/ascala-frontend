8740308357bbd738e38eee8cc4d7d1724c185871
Haider010 <haiderkarar.dev@gmail.com>
2026-06-18 18:15:02 +0500

    feat: Implement hideGHLHeader utility to remove GHL header from the DOM

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
+import { hideGhlHeader } from "./utils/hideGhlHeader";
 import "./styles.css";
+
+hideGhlHeader();
 
 createRoot(document.getElementById("root")).render(<App />);
